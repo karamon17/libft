b8e1c4d790e8db3448020a8b6f37a6546ecd7e27
Gevorg Khaishbashev <gkhaishb@c2r13s4.42yerevan.am>
2023-01-23 20:25:15 +0400

    7th

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -14,11 +14,11 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	size_t c;
-	size_t d;
+	size_t	c;
+	size_t	d;
 
 	if (dstsize <= ft_strlen(dst))
-		return (dstsize + ft_strlen(src));
+		return (dstsize + ft_strlen((char *)src));
 	c = ft_strlen(dst);
 	d = 0;
 	while (src[d] != '\0' && c + 1 < dstsize)
@@ -28,7 +28,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 		d++;
 	}
 	dst[c] = '\0';
-	return (ft_strlen(dst) + ft_strlen(&src[d]));
+	return (ft_strlen(dst) + ft_strlen((char *)&src[d]));
 }
 /*int main()
 {
--- libft.h
+++ libft.h
@@ -18,16 +18,19 @@
 
 size_t	ft_strlen(char *str);
 void	ft_bzero(void *s, size_t n);
-int	ft_isalnum(int arg);
-int	ft_isalpha(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int arg);
+int		ft_isalnum(int arg);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int arg);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memset(void *str, int c, size_t n);
-int	ft_isdigit(int arg);
+int		ft_isdigit(int arg);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+char 	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
 
 #endif
